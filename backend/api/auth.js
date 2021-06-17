c0e7b9c19091d029173fd1e83d9cb190b187196d
camposyuri <yurinapoleao@gmail.com>
2021-06-17 19:39:59 -0300

    Created endpoint /signin and /validateToken

--- backend/api/auth.js
+++ backend/api/auth.js
@@ -1,4 +1,6 @@
+const { authSecret } = require("./../.env");
 const bcrypt = require("bcrypt-nodejs");
+const jwt = require("jwt-simple");
 
 module.exports = (app) => {
   const signin = async (req, res) => {
@@ -7,7 +9,6 @@ module.exports = (app) => {
     }
 
     const user = await app.db("users").where({ email: req.body.email }).first();
-    console.log(user);
 
     if (!user) {
       return res.status(400).send("User not found");
@@ -18,10 +19,45 @@ module.exports = (app) => {
       return res.status(400).send("Incorrect username or password");
     }
 
-    return res.send("Tudo ok!");
+    const now = Math.floor(Date.now() / 1000);
+
+    const payload = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      admin: user.admin,
+      iat: now,
+      exp: now + 60 * 60 * 24 * 3,
+    };
+
+    return res.json({
+      ...payload,
+      token: jwt.encode(payload, authSecret),
+    });
+  };
+
+  const validateToken = async (req, res) => {
+    const userData = req.body || null;
+
+    try {
+      if (userData) {
+        const token = await jwt.decode(userData.token, authSecret);
+        if (new Date(token.exp * 1000) > new Date()) {
+          return res.status(200).send("Token is valid");
+        }
+      }
+    } catch (error) {
+      console.log(
+        `Error: [api][auth][validateToken] >> error with token validate]: ${error}`
+      );
+      return res.status(500).send("Token is not valid");
+    }
+    return res.status(500).send(false);
   };
 
   return {
     signin,
+    validateToken,
   };
 };
--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,5 +1,6 @@
 module.exports = (app) => {
   app.post("/signin", app.api.auth.signin);
+  app.post("/validateToken", app.api.auth.validateToken);
   app.route("/users").get(app.api.user.get).post(app.api.user.save);
   app
     .route("/users/:id")
